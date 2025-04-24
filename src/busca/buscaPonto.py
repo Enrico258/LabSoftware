d7b6a82d0d195ed22d3757da3e239a068487eb2c
Enrico Cuono <105428037+Enrico258@users.noreply.github.com>
2025-04-24 19:40:29 -0300

    Update buscaPonto.py

--- src/busca/buscaPonto.py
+++ src/busca/buscaPonto.py
@@ -1,14 +1,13 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-def buscarPontos():
+def buscarPontos(descarte):
     cred = credentials.Certificate("chave.json")
     firebase_admin.initialize_app(cred)
 
-    db = firestore.client(descarte)
+    db = firestore.client()
 
     colecao = "pontos"
-    #descarte = input("Digite um material:")
 
     enderecos = []
 
@@ -18,3 +17,5 @@ def buscarPontos():
         dados = doc.to_dict()
         if "descarte" in dados and descarte in dados["descarte"]:
             enderecos.append(dados.get("local"))
+
+    return (enderecos)
