fbe5357aee3eccdb6a782920640c895ec06e0723
Enrico Cuono <105428037+Enrico258@users.noreply.github.com>
2025-04-24 19:49:58 -0300

    Update test_busca.py

--- tests/test_busca.py
+++ tests/test_busca.py
@@ -1,4 +1,8 @@
 from busca.buscaPonto import buscarPontos
 
 def test_somar():
-    assert buscarPontos("papel") == ['R. Bairi, 435', 'Rua Anita Garibaldi, 30']
+    print("🔍 Iniciando teste...")
+    resultado = buscarPontos("papel")
+    print("✅ Resultado recebido:", resultado)
+    
+    assert resultado == ['R. Bairi, 435', 'Rua Anita Garibaldi, 30']
