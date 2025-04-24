69a7fa7255ea7b06bf9f46795aa839db57ef3c53
Enrico Cuono <105428037+Enrico258@users.noreply.github.com>
2025-04-24 19:37:01 -0300

    Update buscaPonto.py

--- src/busca/buscaPonto.py
+++ src/busca/buscaPonto.py
@@ -1,5 +1,20 @@
-def somar(a, b):
-    return a + b
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-def subtrair(a, b):
-    return a - b
+def buscarPontos():
+    cred = credentials.Certificate("chave.json")
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client(descarte)
+
+    colecao = "pontos"
+    #descarte = input("Digite um material:")
+
+    enderecos = []
+
+    docs = db.collection(colecao).stream()
+
+    for doc in docs:
+        dados = doc.to_dict()
+        if "descarte" in dados and descarte in dados["descarte"]:
+            enderecos.append(dados.get("local"))
