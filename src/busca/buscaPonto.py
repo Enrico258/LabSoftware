1a8d917462cb80286e9842bb89767aea8d231584
Enrico Cuono <105428037+Enrico258@users.noreply.github.com>
2025-04-25 08:54:37 -0300

    Update buscaPonto.py

--- src/busca/buscaPonto.py
+++ src/busca/buscaPonto.py
@@ -1,9 +1,11 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("chave.json")
+    cred_path = os.getenv("FIREBASE_KEY_PATH", "chave.json")
+    cred = credentials.Certificate(cred_path)
     firebase_admin.initialize_app(cred)
     
 db = firestore.client()
